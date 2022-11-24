fb89e2bc7e6726fea4e7c2cb1341dd395a1ed4ef
Shane Barringer <shanebarringer@gmail.com>
2022-11-24 12:25:48 -0500

    adds API layer and scaffolds frontend

---
+++ src/pages/Index.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Index = () => {
+  const [dinos, setDinos] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:8000/api/`)
+      .then(async (res) => await res.json())
+      .then((json) => setDinos(json));
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome to the deno-saur app</h1>
+      <p>
+        Click on a dinosaur below to learn more.
+      </p>
+      <div>
+        {dinos.map((dino) => 
+            <div>
+              <Link to={`/${dino.name.toLowerCase()}`}>{dino.name}</Link>
+            </div>
+          )
+        }
+      </div>
+    </div>
+  );
+};
+
+export default Index;
